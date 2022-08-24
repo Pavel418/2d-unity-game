59dc5098394ed2349f49fcd73638fec795f2f606
Pavel418 <pavel@vvv.az>
2022-08-24 23:09:37 +0400

    Update FlyBullet.cs

--- Assets/Scripts/FlyBullet.cs
+++ Assets/Scripts/FlyBullet.cs
@@ -10,7 +10,7 @@ public class FlyBullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-            
+        StartCoroutine(nameof(WaitTillDestroy));
     }
 
     // Update is called once per frame
